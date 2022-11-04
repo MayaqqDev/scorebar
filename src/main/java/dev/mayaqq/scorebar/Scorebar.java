2eee4ef5817054ce9d75719ab1e7c570e2e7701c
Maximusbarcz <maxim.baranek@gmail.com>
2022-11-04 21:41:35 +0100

    Optimized imports and changed the icon.png to match modrinth.

--- src/main/java/dev/mayaqq/scorebar/Scorebar.java
+++ src/main/java/dev/mayaqq/scorebar/Scorebar.java
@@ -1,11 +1,6 @@
 package dev.mayaqq.scorebar;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import dev.mayaqq.scorebar.integrations.ModMenuIntegration;
 import net.fabricmc.api.ModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Scorebar implements ModInitializer {
 	@Override
